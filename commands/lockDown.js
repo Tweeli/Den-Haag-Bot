2a27411066722150adc78f3fc8ec630186ce14ac
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-02 22:21:53 +0200

    Update lockDown.js

--- commands/lockDown.js
+++ commands/lockDown.js
@@ -7,15 +7,9 @@ module.exports.run = async (bot, message, args) => {
     await message.channel.overwritePermissions([
 
         {
-            id: message.guild.roles.cache.find(r => r.name == "@everyone").id,
             id: message.guild.roles.cache.find(r => r.name == "Moderator | HR (PROEF)").id,
             id: message.guild.roles.cache.find(r => r.name == "🟦 | Moderator | HR").id,
             id: message.guild.roles.cache.find(r => r.name == "🟩 | Administrator | HR").id,
-            id: message.guild.roles.cache.find(r => r.name == "🟥 | Super Administrator | HC").id,
-            id: message.guild.roles.cache.find(r => r.name == "🟪 | Hoofd Administrator|HC+").id,
-            id: message.guild.roles.cache.find(r => r.name == "🟫 | Manager | TM").id,
-            id: message.guild.roles.cache.find(r => r.name == "Bestuurs-Voorzitter").id,
-            id: message.guild.roles.cache.find(r => r.name == "Gedelegeerd-Bestuurder | CEO").id,
             deny: ['SEND_MESSAGES']
         }
 
