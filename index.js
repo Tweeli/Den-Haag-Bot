79f1e242903547b2e6884eda8e9305792ac278ee
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-02 21:10:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ fs.readdir('./commands/', (err, files) => {
 	}
 
 	jsFiles.forEach((f, i) => {
-		
+
 		var fileGet = require(`./commands/${f}`);
 		console.log(`De file ${f} is geladen.`);
 
@@ -37,6 +37,30 @@ fs.readdir('./commands/', (err, files) => {
 		})
 	});
 });
+
+
+bot.on("guildMemberAdd", member => {
+
+	var role = member.guild.roles.cache.get('871828836529098812');
+
+	if (!role) return;
+
+	member.roles.add(role);
+
+	var channel = member.guild.channels.cache.get('669284473711362088');
+
+	if (!channel) return;
+
+	var welkomEmbed = new discord.MessageEmbed()
+		.setTitle("Welkom!")
+		.setDescription(`Welkom in TeamDJD | Den Haag Stad V2 ${member} \nlees zeker even de #「🟢」discord-regels door!
+		Bij vragen kunt u altijd naar 「🤖」bot-commands gaan en !new typen om een ticket aan te maken!`)
+		.setColor('#6aa75e')
+		.setFooter("Veel plezier toegewenst in de server! | Created by Tweeli.#0001");
+		message.lineReply(welkomEmbed);
+
+})
+
 
 //Bot Status.
 bot.on('ready', async () => {
@@ -50,63 +74,67 @@ bot.on('ready', async () => {
 bot.on("messageDelete", messageDeleted => {
 
 	if (messageDeleted.author.bot) return;
-	
+
 	var content = messageDeleted.content;
 	if (!content) content = "Geen tekst meegegeven.";
 
 	var respone = `Bericht ${messageDeleted.id} is verwijderd uit ${messageDeleted.channel} \n **Bericht:** ${content}`;
 
 	var deletedContentEmbed = new discord.MessageEmbed()
-		.setAuthor(`${messageDeleted.author.tag} (${messageDeleted.author.id})`, `${messageDeleted.author.avatarURL({size: 4096})}`)
+		.setAuthor(`${messageDeleted.author.tag} (${messageDeleted.author.id})`, `${messageDeleted.author.avatarURL({ size: 4096 })}`)
 		.setDescription(respone)
 		.setTimestamp()
 		.setColor('#6aa75e')
 		.setFooter('Created by Tweeli.#0001');
-	
-		bot.channels.cache.get('871799202055868477').send(deletedContentEmbed);
+
+	bot.channels.cache.get('871799202055868477').send(deletedContentEmbed);
 
 
 });
 
-bot.on("messageUpdate", async(oldMessage, newMessage) => {
+
+//Geëdit bericht log.
+bot.on("messageUpdate", async (oldMessage, newMessage) => {
 
 	if (newMessage.author.bot) return;
 
+	if (oldMessage.content == newMessage.content) return;
+
 	var newMessageEmbed = new discord.MessageEmbed()
-		.setAuthor(`${newMessage.author.tag} (${newMessage.author.id})`, newMessage.author.avatarURL({size: 4096}))
+		.setAuthor(`${newMessage.author.tag} (${newMessage.author.id})`, newMessage.author.avatarURL({ size: 4096 }))
 		.setDescription(`Bericht ${newMessage.id} is bewerkt in ${newMessage.channel}\n **Voor:** ${oldMessage.content}\n **Na:** ${newMessage.content}`)
 		.setColor('#6aa75e')
 		.setTimestamp()
 		.setFooter('Created by Tweeli.#0001')
-		bot.channels.cache.get('871799202055868477').send(newMessageEmbed);
+	bot.channels.cache.get('871799202055868477').send(newMessageEmbed);
 
 })
 
 
 //Scheldwoorden/bot.
 bot.on('message', async message => {
-	
-  if (message.author.bot) return;
 
-  if (message.channel.type === "dm") return message.lineReply("Bot commands kunnen niet in dm uitgevoerd worden.")
+	if (message.author.bot) return;
 
-  var swearWords = JSON.parse(fs.readFileSync("./data/swearWords.json"))
+	if (message.channel.type === "dm") return message.lineReply("Bot commands kunnen niet in dm uitgevoerd worden.")
 
-  
+	var swearWords = JSON.parse(fs.readFileSync("./data/swearWords.json"))
+
+
 
 	var msg = message.content.toLocaleLowerCase();
 
-  for (let i =0; i < swearWords["vloekwoorden"].length; i++) {
+	for (let i = 0; i < swearWords["vloekwoorden"].length; i++) {
 
-    if (msg.includes(swearWords["vloekwoorden"][i])) {
-		if (message.member.hasPermission("MANAGE_MESSAGES")) return
+		if (msg.includes(swearWords["vloekwoorden"][i])) {
+			if (message.member.hasPermission("MANAGE_MESSAGES")) return
 
-      message.delete();
+			message.delete();
 
-      return message.reply("Gelieve niet te schelden.").then(msg => msg.delete({ timeout: 3000 }))
-    }
+			return message.reply("Gelieve niet te schelden.").then(msg => msg.delete({ timeout: 3000 }))
+		}
 
-  }
+	}
 
 	var prefix = botConfig.prefix;
 
@@ -114,7 +142,7 @@ bot.on('message', async message => {
 
 	var command = messageArray[0];
 
-  RandomXp(message)
+	RandomXp(message)
 
 	if (!message.content.startsWith(prefix)) return;
 
@@ -129,49 +157,49 @@ bot.on('message', async message => {
 //Level Systeem.
 function RandomXp(message) {
 
-  var randomNumber = Math.floor(Math.random() * 15) + 1;
+	var randomNumber = Math.floor(Math.random() * 15) + 1;
 
-  var idUser = message.author.id
+	var idUser = message.author.id
 
-  if (!levelFile[idUser]) {
-    levelFile[idUser] = {
-      xp: 0,
-      level: 0
-    }
-  }
+	if (!levelFile[idUser]) {
+		levelFile[idUser] = {
+			xp: 0,
+			level: 0
+		}
+	}
 
 
 
- levelFile[idUser].xp += randomNumber
+	levelFile[idUser].xp += randomNumber
 
- var levelUser = levelFile[idUser].level;
- var xpUser = levelFile[idUser].xp;
+	var levelUser = levelFile[idUser].level;
+	var xpUser = levelFile[idUser].xp;
 
- var nextLevelXp = levelUser * 300;
+	var nextLevelXp = levelUser * 300;
 
- if (nextLevelXp === 0) nextLevelXp = 100;
+	if (nextLevelXp === 0) nextLevelXp = 100;
 
- if (xpUser >= nextLevelXp){
+	if (xpUser >= nextLevelXp) {
 
-  levelFile[idUser].level += 1;
+		levelFile[idUser].level += 1;
 
-  fs.writeFile("./data/levels.json", JSON.stringify(levelFile), err =>{
-    if(err) console.log(err)
-  });
+		fs.writeFile("./data/levels.json", JSON.stringify(levelFile), err => {
+			if (err) console.log(err)
+		});
 
- var levelEmbedSend = message.member.guild.channels.cache.get("661970226551717959");
+		var levelEmbedSend = message.member.guild.channels.cache.get("661970226551717959");
 
- var levelEmbed = new discord.MessageEmbed()
-    .setDescription(`** <@${idUser}> is een level hoger!**`)
-    .setColor("#6aa75e")
-    .addField("Nieuw level:", levelFile[idUser].level)
- levelEmbedSend.send(levelEmbed)
+		var levelEmbed = new discord.MessageEmbed()
+			.setDescription(`** <@${idUser}> is een level hoger!**`)
+			.setColor("#6aa75e")
+			.addField("Nieuw level:", levelFile[idUser].level)
+		levelEmbedSend.send(levelEmbed)
+
+	}
 
 }
 
-}
 
 
-	
 
 bot.login(process.env.token);
