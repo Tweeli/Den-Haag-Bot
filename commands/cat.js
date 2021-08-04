b02fb182c0aea7e7c841e4fc4a6c94ee3780b01d
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-04 21:21:59 +0200

    Update!

--- commands/cat.js
+++ commands/cat.js
@@ -1,19 +1,29 @@
 const discord = require("discord.js");
+const fetch = require('node-fetch');
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args) => { 
 
-    const fetch = require('node-fetch');
+    fetch('https://www.reddit.com/r/cats/random/.json').then(resp => resp.json()).then(respOmgevormd => {
 
-    const subreddits = [
-        'cat',
-        'cats',
-        'catpics',
-        'kittens'
-    ]
+        var permaLink = respOmgevormd[0].data.children[0].data.permaLink;
+        var catUrl = `https://www.reddit.com${permaLink}`;
+        var catFoto = respOmgevormd[0].data.children[0].data.url;
+        var catTitle = respOmgevormd[0].data.children[0].data.title;
+
+        var memeEmbed = new discord.MessageEmbed()
+            .setTitle(`${catTitle}`)
+            .setURL(`${catUrl}`)
+            .setImage(`${catFoto}`)
+            .setColor('#6aa75e');
+        message.lineReply(memeEmbed);
+
+    }).catch("error", (err) => {
+        console.log(err.message);
+    })
 
 }
 
 module.exports.help = {
-    name: "catt",
+    name: "cat",
     aliases: ["poes"]
 }
--- commands/dog.js
+++ commands/dog.js
@@ -1,15 +1,25 @@
 const discord = require("discord.js");
+const fetch = require('node-fetch');
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args) => { 
 
-    const fetch = require('node-fetch');
+    fetch('https://www.reddit.com/r/lookatmydog/random/.json').then(resp => resp.json()).then(respOmgevormd => {
 
-    const subreddits = [
-        'dog',
-        'dogs',
-        'dogpics',
-        'puppies'
-    ]
+        var permaLink = respOmgevormd[0].data.children[0].data.permaLink;
+        var dogUrl = `https://www.reddit.com${permaLink}`;
+        var dogFoto = respOmgevormd[0].data.children[0].data.url;
+        var dogTitle = respOmgevormd[0].data.children[0].data.title;
+
+        var memeEmbed = new discord.MessageEmbed()
+            .setTitle(`${dogTitle}`)
+            .setURL(`${dogUrl}`)
+            .setImage(`${dogFoto}`)
+            .setColor('#6aa75e');
+        message.lineReply(memeEmbed);
+
+    }).catch("error", (err) => {
+        console.log(err.message);
+    })
 
 }
 
