38bb50818ea26d3926a0141a4e1203f6232216f9
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-04 21:09:42 +0200

    Update.

---
+++ commands/cat.js
@@ -0,0 +1,19 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    const fetch = require('node-fetch');
+
+    const subreddits = [
+        'cat',
+        'cats',
+        'catpics',
+        'kittens'
+    ]
+
+}
+
+module.exports.help = {
+    name: "catt",
+    aliases: ["poes"]
+}
---
+++ commands/dog.js
@@ -0,0 +1,19 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    const fetch = require('node-fetch');
+
+    const subreddits = [
+        'dog',
+        'dogs',
+        'dogpics',
+        'puppies'
+    ]
+
+}
+
+module.exports.help = {
+    name: "dog",
+    aliases: ["hond"]
+}
