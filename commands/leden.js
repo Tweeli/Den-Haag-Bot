f3bf208bb42f988e265533ab73115c14e3063623
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-05 17:03:55 +0200

    Update leden.js

--- commands/leden.js
+++ commands/leden.js
@@ -10,8 +10,8 @@ module.exports.run = async (bot, message, args) => {
     var ledenEmbed = new discord.MessageEmbed()
         .setColor("#6aa75e")
         .addField("Leden | 👤", ledenTotal, true)
-        .addField("Bots | 🤖", `${message.guild.members.cache.filter(m =>m.user.bot).size}`, true)
         .addField("Mensen | 👤", `${message.guild.memberCount - message.guild.members.cache.filter(m =>m.user.bot).size}`, true)
+        .addField("Bots | 🤖", `${message.guild.members.cache.filter(m =>m.user.bot).size}`, true)
         .addField("Totaal aantal leden | 👥", message.guild.memberCount, true)
         .addField("Online | 🟢", `${message.guild.members.cache.filter(m =>m.user.presence.status == "online" || m.user.presence.status == "dnd" || m.user.presence.status == "idle").size}`, true)
         .setFooter('TeamDJD | Den Haag Stad V2', 'https://cdn.discordapp.com/attachments/755878713668796446/872847136478351380/image0.png');
