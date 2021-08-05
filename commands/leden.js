f5399e98d58c9f5f23cab2a6b917f7ed3770fd20
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-05 16:59:37 +0200

    Joe

--- commands/leden.js
+++ commands/leden.js
@@ -9,11 +9,11 @@ module.exports.run = async (bot, message, args) => {
 
     var ledenEmbed = new discord.MessageEmbed()
         .setColor("#6aa75e")
-        .addField("Leden", ledenTotal, true)
-        .addField("Bot(s):", `${message.guild.members.cache.filter(m =>m.user.bot).size}`, true)
-        .addField("Mensen:", `${message.guild.memberCount - message.guild.members.cache.filter(m =>m.user.bot).size}`, true)
-        .addField("Totaal aantal leden:", message.guild.memberCount, true)
-        .addField("Online:", `${message.guild.members.cache.filter(m =>m.user.presence.status == "online" || m.user.presence.status == "dnd" || m.user.presence.status == "idle").size}`, true)
+        .addField("Leden | 👤", ledenTotal, true)
+        .addField("Bots | 🤖", `${message.guild.members.cache.filter(m =>m.user.bot).size}`, true)
+        .addField("Mensen | 👤", `${message.guild.memberCount - message.guild.members.cache.filter(m =>m.user.bot).size}`, true)
+        .addField("Totaal aantal leden | 👥", message.guild.memberCount, true)
+        .addField("Online | 🟢", `${message.guild.members.cache.filter(m =>m.user.presence.status == "online" || m.user.presence.status == "dnd" || m.user.presence.status == "idle").size}`, true)
         .setFooter('TeamDJD | Den Haag Stad V2', 'https://cdn.discordapp.com/attachments/755878713668796446/872847136478351380/image0.png');
 
     message.lineReply(ledenEmbed);
