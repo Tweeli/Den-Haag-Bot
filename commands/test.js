e3226d6d82aad3e13e7e8bf21e08a312e1470fe4
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-08 12:43:05 +0200

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,15 @@
+const discord = require("discord.js");
+const moment = require("moment");
+
+module.exports.run = async(bot, message, args) => {
+
+    var gementionedUser = message.guild.member( message.mentions.users.first() || message.guild.members.get(args[1]));
+   
+    console.log(gementionedUser)
+
+}
+
+module.exports.help = {
+    name: "test",
+    aliases: []
+}
