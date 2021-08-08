baa2a5b1a993ef83421bd8f72e7ac36194aa7f58
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-08 11:43:01 +0200

    Update aanmelden.js

--- commands/aanmelden.js
+++ commands/aanmelden.js
@@ -6,7 +6,9 @@ module.exports.run = async (bot, message, args) => {
 
     var afmelden = message.member.guild.channels.cache.get("666690472692940830");
 
-    
+    var afmeldRole = message.guild.roles.cache.get('873857897136783360');
+
+    message.member.roles.remove(afmeldRole.id);
 
     afmelden.send(`${message.author}, u bent weer aangemeld!`);
     return message.lineReply("U bent succesvol aangemeld.")
