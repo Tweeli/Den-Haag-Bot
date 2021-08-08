bd6d0e1ce10564ca8fbb19b74e4236fa0a6c4c14
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-08 14:31:42 +0200

    Update quotes.js

--- commands/quotes.js
+++ commands/quotes.js
@@ -9,9 +9,9 @@ module.exports.run = async (bot, message, args) => {
     var num = Math.floor(Math.random()*quotes.Quotes.length)
 
     var quoteEmbed = new discord.MessageEmbed()
-     .setDescription(quotes.Quotes[num].q + "\n- " + quotes.Quotes[num].a)
+     .setDescription(quotes.Quotes[num].q)
      .setColor("#6aa75e")
-     .setFooter('TeamDJD | Den Haag Stad V2', 'https://cdn.discordapp.com/attachments/755878713668796446/872847136478351380/image0.png');
+     .setFooter(quotes.Quotes[num].a);
     message.lineReply(quoteEmbed);
 
 }
