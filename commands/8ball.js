4a3f48af2b7b69bb15e8b2b5b601b93ef1c6a4f5
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-09 23:02:17 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 	
-    if(message.channel.id !== "560844017336582144" || message.channel.id !== "692811822809743380") return message.lineReply('> Bot commands kunnen alleen uitgevoerd worden in <#560844017336582144>.');
+    if(!message.channel.id == "560844017336582144" || message.channel.id == "692811822809743380") return message.reply('Gaat niet.');
 
 
 
