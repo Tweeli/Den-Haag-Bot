1d5c7f4301cb6df384e0d54f040aa0c2042f87f6
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-08-09 15:33:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -194,8 +194,6 @@ bot.on('ready', async () => {
 		if (command == "commands") {
 			// [{name: 'inhoud', value: "tekst meegeeft"}]
 
-			if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Je hebt geen toestemming om dit te doen.");
-
 			var staffCommandsEmbed = new discord.MessageEmbed()
 				.setTitle("Den Haag Bot Staff Commands:")
 				.setDescription("> !aanmelden - Je meld je weer aan. \n > !afmelden - Je meld je af. \n > !ban - Je bant de opgegeven gebruiker van de server.\n > !clear - Je delete het aantal berichten dat je hebt opgegeven. \n > !kick - Je kickt de opgegeven gebruiker van de server. \n > !mute - Mute de gebruiker voor zolang dat jij opgegeven hebt. > !pm - De bot dm't de gebruiker met het bericht dat jij opgegeven hebt. \n > !remove - Verwijderd de gebruiker uit een ticket. > !say - De bot zegt in dezelfde chat het bericht dat jij hebt meegegeven. \n > !staff-commands - Geeft alle commands weer die staff-members meer kunnen uitvoeren dan spelers. \n > !staff-suggestie - Met deze command vraag je een staff-suggestie aan. \n > !close - closed een ticket. \n > !warn - Je warnd de gebruiker die je hebt opgegeven.")
